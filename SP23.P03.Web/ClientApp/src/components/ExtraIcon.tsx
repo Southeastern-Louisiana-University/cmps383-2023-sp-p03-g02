24245741d376c5956bb0814c3ed7f39d58de8fb7
Jared Talbot <jared.talbot-2@selu.edu>
2023-02-26 04:50:30 -0600

    Adds Semantic UI and component for extra icons
    by "extra icons" I mean icons that don't exist on the old version of fontawesome that semantic ui is still using for some reason

---
+++ SP23.P03.Web/ClientApp/src/components/ExtraIcon.tsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { faRunning, faWalking } from "@fortawesome/free-solid-svg-icons";
+
+// This component is for FontAwesome icons that are not included in Semantic UI's Icon component.
+// Feel free to add more icons if you need them.
+
+const EXTRA_ICONS = {
+    "running": faRunning,
+    "walking": faWalking,
+};
+
+type ExtraIconProps = Omit<FontAwesomeIconProps, "icon"> & {
+    name: keyof typeof EXTRA_ICONS;
+}
+
+
+const ExtraIcon = (props: ExtraIconProps) => {
+    const { name, ...rest } = props;
+    return <FontAwesomeIcon icon={EXTRA_ICONS[name]} {...rest} />;
+}
+
+export default ExtraIcon;
