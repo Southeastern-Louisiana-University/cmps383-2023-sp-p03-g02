80fe062aaaf5eaa0e5ce83d05299b4af429af060
Zoe Klein <zoe.klein@selu.edu>
2023-03-02 17:43:09 -0600

    TrainDto and CreateTrainDto

---
+++ SP23.P03.Web/Features/Trains/TrainDto.cs
@@ -0,0 +1,18 @@
+﻿namespace SP23.P03.Web.Features.Trains
+{
+    public class TrainDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public int Capacity { get; set; }
+    }
+
+    public class CreateTrainDto 
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public int Capacity { get; set; }
+
+    }
+}
