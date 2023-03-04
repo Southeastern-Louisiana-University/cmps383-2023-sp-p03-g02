1cce6e53fd63d1dbe58f37499ff96423263b08dc
Jared Talbot <jared.talbot-2@selu.edu>
2023-03-03 19:08:19 -0600

    feat; Add Passenger config and User relationship

---
+++ SP23.P03.Web/Features/Passengers/PassengerConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SP23.P03.Web.Features.Passengers
+{
+    public class PassengerConfiguration: IEntityTypeConfiguration<Passenger>
+    {
+        public void Configure(EntityTypeBuilder<Passenger> builder)
+        {
+            builder.Property(x => x.Owner)
+                   .IsRequired();
+
+            builder.Property(x => x.FirstName)
+                   .HasMaxLength(64)
+                   .IsRequired();
+
+            builder.Property(x => x.LastName)
+                   .HasMaxLength(64)
+                   .IsRequired();
+
+            builder.Property(x => x.Birthday)
+                   .IsRequired();
+
+            builder.HasOne(x => x.Owner)
+                   .WithMany(x => x.OwnedPassengers)
+                   .HasForeignKey(x => x.OwnerId);
+        }
+    }
+}
