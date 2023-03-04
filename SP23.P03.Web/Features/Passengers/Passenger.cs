b137a185be607fb2209eb038cc750382495e3c25
Jared Talbot <jared.talbot-2@selu.edu>
2023-03-03 19:06:59 -0600

    feat; Add Passenger entity

---
+++ SP23.P03.Web/Features/Passengers/Passenger.cs
@@ -0,0 +1,15 @@
+﻿using SP23.P03.Web.Features.Authorization;
+
+namespace SP23.P03.Web.Features.Passengers
+{
+    public class Passenger
+    {
+        public int Id { get; set; }
+        public int OwnerId { get; set; }
+        public User Owner { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTimeOffset Birthday { get; set; }
+
+    }
+}
