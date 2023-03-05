de8328b8c0dff0f6a69780b96efc1fd6a4577c5e
Zoe Klein <zoe.klein@selu.edu>
2023-03-04 19:34:16 -0600

    feat; setup TrainsController

---
+++ SP23.P03.Web/Controllers/TrainsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SP23.P03.Web.Data;
+using SP23.P03.Web.Extensions;
+using SP23.P03.Web.Features.Authorization;
+using SP23.P03.Web.Features.Trains;
+using SP23.P03.Web.Features.TrainStations;
+
+namespace SP23.P03.Web.Controllers
+{
+    [Route("api/trains")]
+    [ApiController]
+    public class TrainsController : ControllerBase
+    {
+        private readonly DbSet<Train> trains;
+        private readonly DataContext dataContext;
+
+        public TrainsController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+            trains = dataContext.Set<Train>();
+        }
+    }
+}
