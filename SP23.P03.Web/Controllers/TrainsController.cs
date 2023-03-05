98f67f828fd0eafe0203c99364766a6e117c3628
Zoe Klein <zoe.klein@selu.edu>
2023-03-04 19:56:08 -0600

    HttpGet; return train by id

--- SP23.P03.Web/Controllers/TrainsController.cs
+++ SP23.P03.Web/Controllers/TrainsController.cs
@@ -21,5 +21,23 @@ namespace SP23.P03.Web.Controllers
             this.dataContext = dataContext;
             trains = dataContext.Set<Train>();
         }
-    }
-}
+
+        [HttpGet("{id}")]
+        [Authorize]
+        public ActionResult<TrainDto> GetTrainById([FromRoute] int id)
+        {
+            var train = trains.FirstOrDefault(x => x.Id == id);
+
+            var trainDto = new TrainDto
+            {
+                Id = train.Id,
+                Name = train.Name,
+                Status = train.Status,
+                Capacity = train.Capacity
+            };
+
+            return Ok(trainDto);
+        }
+
+    }//end TrainsController
+}//end namespace
