7db86af98f7bda5806a8c0ee5ed4687828d5ce6f
Jacob Poirrier <jakep0802@gmail.com>
2023-03-06 14:41:56 -0600

    wip: Add GET methods for Trips controller

---
+++ SP23.P03.Web/Controllers/TripsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SP23.P03.Web.Data;
+using SP23.P03.Web.Features.Trips;
+
+namespace SP23.P03.Web.Controllers;
+
+[Route("api/trips")]
+[ApiController]
+
+public class TripsController : ControllerBase
+{
+    private readonly DbSet<Trip> trips;
+    private readonly DataContext dataContext;
+
+    public TripsContoller(DataContext dataContext)
+    {
+        this.dataContext = dataContext;
+        trips = dataContext.Set<Trip>();
+    }
+
+    [HttpGet]
+    public IQueryable<TripDto> GetAllTrips()
+    {
+        return GetTripDtos(trips);
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public ActionResult<TripDto> GetTripById(int id)
+    {
+        var result = GetTripDtos(trips.Where(x => x.Id == id)).FirstOrDefault();
+        if (result == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+}
