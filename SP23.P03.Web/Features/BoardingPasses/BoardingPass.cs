c614d82289e5f30751edeca96b146fb492dcf82c
Jared Talbot <jared.talbot-2@selu.edu>
2023-03-08 15:00:10 -0600

    feat; Boarding pass entity + config

---
+++ SP23.P03.Web/Features/BoardingPasses/BoardingPass.cs
@@ -0,0 +1,17 @@
+﻿using SP23.P03.Web.Features.Authorization;
+using SP23.P03.Web.Features.Passengers;
+using SP23.P03.Web.Features.Trips;
+
+namespace SP23.P03.Web.Features.BoardingPasses;
+
+public class BoardingPass
+{
+    public int Id { get; set; }
+    public string Code { get; set; } = string.Empty;
+    public int OwnerId { get; set; }
+    public User Owner { get; set; }
+    public int TripId { get; set; }
+    public Trip Trip { get; set; }
+    public ICollection<Passenger> Passengers { get; set; } = new List<Passenger>();
+
+}
---
+++ SP23.P03.Web/Features/BoardingPasses/BoardingPassConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace SP23.P03.Web.Features.BoardingPasses
+{
+    public class BoardingPassConfiguration : IEntityTypeConfiguration<BoardingPass>
+    {
+        public void Configure(EntityTypeBuilder<BoardingPass> builder)
+        {
+            builder.Property(x => x.Code)
+                   .HasMaxLength(64)
+                   .IsRequired();
+
+            builder.HasOne(x => x.Owner)
+                   .WithMany()
+                   .HasForeignKey(x => x.OwnerId)
+                   .IsRequired();
+
+            builder.HasOne(x => x.Trip)
+                   .WithMany()
+                   .HasForeignKey(x => x.TripId)
+                   .IsRequired();
+
+            builder.HasMany(x => x.Passengers)
+                   .WithMany()
+                   .UsingEntity<BoardingPassenger>();
+        }
+    }
+}
