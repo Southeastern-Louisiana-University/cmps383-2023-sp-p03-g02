83b241e39996543d53d0205f872db7c80823070c
Jared Talbot <jared.talbot-2@selu.edu>
2023-03-08 15:00:34 -0600

    feat; Boarding passenger entity + config

---
+++ SP23.P03.Web/Features/BoardingPasses/BoardingPassenger.cs
@@ -0,0 +1,11 @@
+﻿using SP23.P03.Web.Features.Passengers;
+
+namespace SP23.P03.Web.Features.BoardingPasses;
+
+public class BoardingPassenger
+{
+    public int BoardingPassId { get; set; }
+    public BoardingPass BoardingPass { get; set; }
+    public int PassengerId { get; set; }
+    public Passenger Passenger { get; set; }
+}
---
+++ SP23.P03.Web/Features/BoardingPasses/BoardingPassengerConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace SP23.P03.Web.Features.BoardingPasses;
+
+public class BoardingPassengerConfiguration : IEntityTypeConfiguration<BoardingPassenger>
+{
+    public void Configure(EntityTypeBuilder<BoardingPassenger> builder)
+    {
+        builder.HasOne(x => x.BoardingPass)
+               .WithMany()
+               .HasForeignKey(x => x.BoardingPassId)
+               .IsRequired();
+
+        builder.HasOne(x => x.Passenger)
+               .WithMany()
+               .HasForeignKey(x => x.PassengerId)
+               .IsRequired();
+    }
+}
