e871aa41046b31292780507f8d6a0aabfe8fa405
Jacob Poirrier <jakep0802@gmail.com>
2023-03-08 14:00:17 -0600

    feat: Add migration for Trip

---
+++ SP23.P03.Web/Migrations/20230308192026_AddTrips.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SP23.P03.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTrips : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trip",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TrainId = table.Column<int>(type: "int", nullable: false),
+                    FromStationId = table.Column<int>(type: "int", nullable: false),
+                    ToStationId = table.Column<int>(type: "int", nullable: false),
+                    Departure = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    Arrival = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    BasePrice = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trip", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Trip_TrainStation_FromStationId",
+                        column: x => x.FromStationId,
+                        principalTable: "TrainStation",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Trip_TrainStation_ToStationId",
+                        column: x => x.ToStationId,
+                        principalTable: "TrainStation",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Trip_Train_TrainId",
+                        column: x => x.TrainId,
+                        principalTable: "Train",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trip_FromStationId",
+                table: "Trip",
+                column: "FromStationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trip_ToStationId",
+                table: "Trip",
+                column: "ToStationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trip_TrainId",
+                table: "Trip",
+                column: "TrainId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trip");
+        }
+    }
+}
