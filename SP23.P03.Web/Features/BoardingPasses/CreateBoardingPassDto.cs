3ed8ae847ea0fb3a31f452b889eecbbe74b5b887
Jared Talbot <jared.talbot-2@selu.edu>
2023-03-08 15:00:57 -0600

    feat; Boarding pass DTOs

---
+++ SP23.P03.Web/Features/BoardingPasses/CreateBoardingPassDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SP23.P03.Web.Features.BoardingPasses;
+
+public class CreateBoardingPassDto
+{
+    public int TripId { get; set; }
+    public ICollection<int> PassengerIds { get; set; } = new List<int>();
+}
