ebc18d72f9cd516c97bb4ad70ea44ca10aa58e2b
Jared Talbot <jared.talbot-2@selu.edu>
2023-03-15 16:15:29 -0500

    feat; Give the home page a more passable look

--- SP23.P03.Web/ClientApp/src/pages/Home/HomePage.tsx
+++ SP23.P03.Web/ClientApp/src/pages/Home/HomePage.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Image } from 'semantic-ui-react';
+import HomeLogo from '../../assets/EnTrack_HomeLogo.png'
+import './HomePage.css';
 
 export function HomePage(): React.ReactElement {
     return (
-        <div>
-            <header> Home Page </header>
+        <div className='home-page'>
+            <header> Let's Get You... </header>
+            
+            <Image src={HomeLogo} size='large' />
         </div>
     );
 }
