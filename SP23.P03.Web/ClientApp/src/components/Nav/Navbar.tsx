85ad654fe57fc52d5aedcf30d771c8fe8810a480
Jared Talbot <jared.talbot-2@selu.edu>
2023-03-15 15:50:05 -0500

    feat; Add new logo to home tab

--- SP23.P03.Web/ClientApp/src/components/Nav/Navbar.tsx
+++ SP23.P03.Web/ClientApp/src/components/Nav/Navbar.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Menu } from "semantic-ui-react"; 
+import { Image, Menu } from "semantic-ui-react"; 
 import { routes } from "../../constants/routeconfig";
+import HomeLogo from '../../assets/EnTrack_HomeLogo.png';
 import "./NavbarStyling.css"; 
 
 export function Navbar() : React.ReactElement {
@@ -13,8 +14,8 @@ export function Navbar() : React.ReactElement {
             className="item-container">
                 
                 <Menu.Item
-                    content='EnTrack'
-                    icon="train"
+                    style={{ padding: 0 }}
+                    content={<Image className="main-logo" src={HomeLogo} />}
                     as={NavLink}
                     to={routes.home}
                 />
