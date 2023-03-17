ad06899ad4d1fd0d6ef4996ddb89fa1fc42ae461
Zoe Klein <zoe.klein@selu.edu>
2023-03-17 01:00:55 -0500

    wip; designing web page

--- SP23.P03.Web/ClientApp/src/pages/Home/HomePage.tsx
+++ SP23.P03.Web/ClientApp/src/pages/Home/HomePage.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
-import { Image } from 'semantic-ui-react';
-import HomeLogo from '../../assets/EnTrack_HomeLogo.png'
+import { Header, Segment } from 'semantic-ui-react';
 import './HomePage.css';
 
 export function HomePage(): React.ReactElement {
     return (
-        <div className='home-page'>
-            <header> Let's Get You... </header>
+        <div>
+            <h2 className="slogan"> 
+                Let's Get You Entrack
+            </h2>
+
+            <div className='info-box-position'>
+            <Segment compact emphasis padded
+            className="info-box">
+                <Header>
+                    Why Choose EnTrack?
+                </Header>
+
+                <p className="info-box-content">
+                We strive to make your experience traveling
+                the best that it can be! At much cheaper prices,
+                and an easier experience, let us be the first
+                option you think of to journey to your next
+                vacation!
+                </p>
+
+            </Segment>
+            </div>
             
-            <Image src={HomeLogo} size='large' />
         </div>
     );
 }
