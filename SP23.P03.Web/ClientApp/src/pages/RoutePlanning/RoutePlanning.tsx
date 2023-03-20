240f2ca86763ff39e7a8be3432c0383108a2810a
Jacob Poirrier <jakep0802@gmail.com>
2023-03-20 15:46:02 -0500

    wip: create axios get call

--- SP23.P03.Web/ClientApp/src/pages/RoutePlanning/RoutePlanning.tsx
+++ SP23.P03.Web/ClientApp/src/pages/RoutePlanning/RoutePlanning.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-export function RoutePlanning() : React.ReactElement {
+export function RoutePlanning(): React.ReactElement {
+    const [trips, setTrips] = useState<any[]>([]);
+
+    useEffect(() => {
+        axios
+            .get(`/api/trips`)
+            .then((response) => {
+                setTrips(response.data);
+                console.log(response);
+            });
+    }, []);
+
     return (
         <div>
-            
+            <h1>Routes</h1>
+            {trips.map((trip) => {
+                return (
+                    <div>
+                        {trip.id}
+                    </div>
+                )
+            })}
         </div>
     );
 }
