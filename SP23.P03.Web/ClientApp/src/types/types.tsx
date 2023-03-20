53dc022a404a41d4004d3ed198a6bd59f00d066a
Jacob Poirrier <jakep0802@gmail.com>
2023-03-20 18:34:44 -0500

    wip: list all routes on route-planning

--- SP23.P03.Web/ClientApp/src/types/types.tsx
+++ SP23.P03.Web/ClientApp/src/types/types.tsx
@@ -2,3 +2,13 @@ export type StationGetDto = {
     id: number;
     Name: string;
 }
+
+export type TripDto = {
+    id: number;
+    trainId: number;
+    fromStationId: number;
+    toStationId: number;
+    departure: Date;
+    arrival: Date;
+    basePrice: number;
+}
