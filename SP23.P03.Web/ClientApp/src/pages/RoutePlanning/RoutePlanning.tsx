818813749ac16a5f63c069f4ab12ef8dc512ad78
Jared Talbot <jared.talbot-2@selu.edu>
2023-03-27 18:06:53 -0500

    chore; Remove unfortunately now-irrelevant header
    (for RoutePlanning)

--- SP23.P03.Web/ClientApp/src/pages/RoutePlanning/RoutePlanning.tsx
+++ SP23.P03.Web/ClientApp/src/pages/RoutePlanning/RoutePlanning.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useMatch } from "react-router-dom";
-import { Divider, Header, Icon } from "semantic-ui-react";
+import { Divider } from "semantic-ui-react";
 import RouteListing from "./RouteListing";
 import './RoutePlanning.css';
 
@@ -10,13 +10,7 @@ export function RoutePlanning(): React.ReactElement {
     return (
         <div>
             <div>
-                <Header className="route-planning-header" as='h2' block>
-                    <Icon name='map marker alternate' />
-                    <Header.Content>
-                        All Routes
-                        <Header.Subheader>List of all routes, sorted by earliest departure date</Header.Subheader>
-                    </Header.Content>
-                </Header>
+                {/* Add future RoutePlanner component here */}
                 <Divider />
             </div>
             <div>
