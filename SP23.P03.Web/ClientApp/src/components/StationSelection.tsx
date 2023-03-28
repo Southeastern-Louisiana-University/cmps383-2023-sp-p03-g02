0e907659b0093e31e69f219a48cf0541ffc9afd9
Zoe Klein <zoe.klein@selu.edu>
2023-03-27 21:35:05 -0500

    wip; moving axios call into own file like the others

--- SP23.P03.Web/ClientApp/src/components/StationSelection.tsx
+++ SP23.P03.Web/ClientApp/src/components/StationSelection.tsx
@@ -1,7 +1,5 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Select } from "semantic-ui-react";
-import { TrainStationDto } from "../types/types";
 
 interface StationSelectProps {
     label: string;
@@ -11,23 +9,6 @@ interface StationSelectProps {
 };
 
 export function StationSelection({ label, placeholder, value, setValue }: StationSelectProps): React.ReactElement {
-    const [stations, setStations] = useState<TrainStationDto[]>([]);
-
-    useEffect (() => {
-        axios
-            .get<TrainStationDto>(`/stations`)
-            .then((response) => {
-                setStations([response.data]);
-                console.log(response.data);
-            });
-    }, []);
-
-    const data = stations.map((option: { name: any; address: any;}) => {
-        return {
-            label: `${option.name} | ${option.address}`,
-            value: option.name,
-        };
-    });
 
     return (
         <>
@@ -37,10 +18,10 @@ export function StationSelection({ label, placeholder, value, setValue }: Statio
                 }}
                 placeholder="Enter City"
                 label={label}
-                options={data}
                 search
-                clearable
-            />
+                clearable 
+                options={[]}
+                />
         </>
     );
 }
