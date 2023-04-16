38132415687364aa879b5bb6ea16af47f223db5b
Zoe Klein <zoe.klein@selu.edu>
2023-04-16 05:40:36 -0500

    wip; allowing for creation of train stations

--- SP23.P03.Web/ClientApp/src/pages/TrainStations/StationListingPage.tsx
+++ SP23.P03.Web/ClientApp/src/pages/TrainStations/StationListingPage.tsx
@@ -1,24 +1,80 @@
 import React from 'react';
-import { Container, Divider, Grid, Segment } from 'semantic-ui-react';
+import { Button, Container, Divider, Grid, Icon, Popup, Segment } from 'semantic-ui-react';
 import StationList from '../../components/StationList';
 import useFindStation from '../../hooks/api/useFindStation';
+import './StationListingPage.css';
+import { useUser } from '../../components/AuthProvider';
+import { CreateStationDto } from '../../types/types';
+import StationDataService from '../../hooks/api/StationDataService';
+import { Form, Formik } from 'formik';
 
 export function StationListingPage(): React.ReactElement {
-
+    const user = useUser();
     const stations = useFindStation();
+    const trainStation = StationDataService(user);
+
+    const onCreate = async (values: CreateStationDto) => {
+        await trainStation.createStation(values);
+    }
+
+    const initialValues: CreateStationDto = {
+        name: "",
+        address: "",
+    };
     
     return (
-        <Container>
+        <Container className="stations">
             <Segment>
-                <Grid.Column>
-                    <h1> Train Stations </h1>
-                </Grid.Column>
 
-                <Divider />
+                {/* 
+                    if user is not signed in & an admin/manager, they can only view the stations
+                    if user meets req, they will be able to create, edit, and delete stations 
+                */}
 
-                <Grid.Row>
-                    <StationList stations={stations} />
-                </Grid.Row>
+                { user ?  ( //this displays when the user is logged in
+                    <Grid columns={2}>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <h1> Train Stations </h1>
+                            </Grid.Column>
+
+                            <Grid.Column>
+                                <Formik initialValues={initialValues} onSubmit={onCreate}>
+                                    <Popup
+                                        as={Form}
+                                        trigger={
+                                            <Button positive compact floated='right'>
+                                                <Icon name="plus" /> Create
+                                            </Button>
+                                        }
+                                        on="click"
+                                    >
+
+                                        <div> popup info here!</div>
+                                    </Popup>
+                                </Formik>
+                            </Grid.Column>
+                        </Grid.Row>
+
+                        <Divider />
+
+                        <Grid.Row>
+                            <StationList stations={stations} />
+                        </Grid.Row>
+                    </Grid>
+                ) : ( //if not -- render just the stations listing
+                    <Grid>
+                        <Grid.Row>
+                            <h1> Train Stations </h1>
+                        </Grid.Row>
+
+                        <Divider />
+                    
+                        <Grid.Row>
+                            <StationList stations={stations} />
+                        </Grid.Row>
+                    </Grid>
+                )}
             </Segment>
         </Container>
     );
