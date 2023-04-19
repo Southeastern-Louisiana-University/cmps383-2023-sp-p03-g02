2a34ac7ed6e5dd83a13257315b37e6f01208ffa4
Zoe Klein <zoe.klein@selu.edu>
2023-04-18 23:25:19 -0500

    feat; admin can delete station (success!)

--- SP23.P03.Web/ClientApp/src/components/StationList.tsx
+++ SP23.P03.Web/ClientApp/src/components/StationList.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { Header, List, ListProps } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Button, Confirm, Header, Icon, List, ListProps } from 'semantic-ui-react';
 import { TrainStationDto } from '../types/types';
+import StationDataService from '../hooks/api/StationDataService';
+import { useUser } from './AuthProvider';
 
 type StationListProps = ListProps & {
     stations: TrainStationDto[];
+}
+
+type StationActionProps = {
+    stations: TrainStationDto[];
+    station: TrainStationDto;
 }
 
 const StationListingPage: React.FC<StationListProps> = (props) => {
@@ -24,10 +31,39 @@ const StationListingPage: React.FC<StationListProps> = (props) => {
 
                         {station.address}
                     </List.Content>
+
+                    <StationDelete stations={stations} station={station} key={station.id} />
                 </List.Item>
             ))}
         </List>
     );
+}
+
+const StationDelete: React.FC<StationActionProps> = ({stations, station}) => {
+    const user = useUser();
+    const trainStation = StationDataService(user);
+    const [open, setOpen] = useState(false);
+
+    const onDelete = async () => {
+        await trainStation.deleteStation(station.id);
+        setOpen(false);
+    }
+
+    return (
+        <Confirm
+            open={open}
+            onOpen={() => setOpen(true)}
+            onClose={() => setOpen(false)}
+            onCancel={() => setOpen(false)}
+            trigger={
+                <Button negative>
+                    <Icon name="trash" /> Delete
+                </Button>
+            }
+            onConfirm={onDelete}
+            content={`Are you sure?`}
+        />
+    )
 }
 
 export default StationListingPage;
