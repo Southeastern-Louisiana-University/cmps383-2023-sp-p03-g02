e13cb979ba724e591a9d59d78b7dd72df37cbb36
Zoe Klein <zoe.klein@selu.edu>
2023-04-19 15:10:10 -0500

    move code; should only list stations

--- SP23.P03.Web/ClientApp/src/components/StationList.tsx
+++ SP23.P03.Web/ClientApp/src/components/StationList.tsx
@@ -1,110 +1,36 @@
-import React, { useState } from 'react';
-import { Button, Confirm, Header, Icon, Input, List, ListProps, Popup } from 'semantic-ui-react';
-import { CreateStationDto, TrainStationDto } from '../types/types';
-import StationDataService from '../hooks/api/StationDataService';
-import { useUser } from './AuthProvider';
-import { Field, Form, Formik } from 'formik';
+import React from 'react';
+import { Grid, Header, List, ListProps } from 'semantic-ui-react';
+import { TrainStationDto } from '../types/types';
 
 type StationListProps = ListProps & {
     stations: TrainStationDto[];
-}
-
-type StationActionProps = {
-    stations: TrainStationDto[];
-    station: TrainStationDto;
 }
 
 const StationListingPage: React.FC<StationListProps> = (props) => {
     const { stations } = props;
 
     return (
-        <List>
-            {stations.map(station => (
-                <List.Item key={station.id}>
-                    <List.Icon>
-                        <i className="map signs icon"/>
-                    </List.Icon>
+        <Grid>
+            <Grid.Column>
+                <List>
+                    {stations.map(station => (
+                        <List.Item key={station.id}>
+                            <List.Icon>
+                                <i className="map signs icon"/>
+                            </List.Icon>
 
-                    <List.Content>
-                        <Header>
-                            {station.name}
-                        </Header>
-
-                        {station.address}
-                    </List.Content>
-
-                    <StationDelete stations={stations} station={station} key={station.id} />
-                    <StationEdit stations={stations} station={station} key={station.id} />
-                </List.Item>
-            ))}
-        </List>
+                            <List.Content>
+                                <Header>
+                                    {station.name}
+                                </Header>
+                                {station.address}
+                            </List.Content>
+                        </List.Item>
+                    ))}
+                </List>
+            </Grid.Column>
+        </Grid>
     );
-}
-
-const StationDelete: React.FC<StationActionProps> = ({station}) => {
-    const user = useUser();
-    const trainStation = StationDataService(user);
-    const [open, setOpen] = useState(false);
-
-    const onDelete = async () => {
-        await trainStation.deleteStation(station.id);
-        setOpen(false);
-    }
-
-    return (
-        <Confirm
-            open={open}
-            onOpen={() => setOpen(true)}
-            onClose={() => setOpen(false)}
-            onCancel={() => setOpen(false)}
-            trigger={
-                <Button negative>
-                    <Icon name="trash" /> Delete
-                </Button>
-            }
-            onConfirm={onDelete}
-            content={`Are you sure?`}
-        />
-    )
-}
-
-const StationEdit: React.FC<StationActionProps> = ({station}) => {
-    const user = useUser();
-    const trainStation = StationDataService(user);
-
-    const onEdit = async (values: CreateStationDto) => {
-        await trainStation.updateStation(station.id, values);
-    }
-
-    const initialValues: CreateStationDto = {
-        name: station.name,
-        address: station.address
-    }
-
-    return (
-        <Formik initialValues={initialValues} onSubmit={onEdit} >
-            <Popup
-                as={Form}
-                trigger={
-                    <Button color="yellow">
-                        <Icon name="edit" /> Edit
-                    </Button>
-                }
-                on='click'     
-            >
-                <div>
-                    <h2> Edit Station </h2>
-                    <label htmlFor="station"><b> Station </b></label>
-                    <Field as={Input} id="name" name="name" />
-
-                    <label htmlFor="address"><b> Address </b></label>
-                    <Field as={Input} id="address" name="address" />
-
-                    <Button type="submit" positive> Submit </Button>
-                </div>
-            </Popup>
-        </Formik>
-    )
 }
 
 export default StationListingPage;
