f3f744361d4d35f2a5cb9c3db1e6c876a6ba91e6
Jared Talbot <jared.talbot-2@selu.edu>
2023-04-19 16:30:22 -0500

    chore; Add migration to delete existing trips

---
+++ SP23.P03.Web/Migrations/20230419212711_WipeTrips.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SP23.P03.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class WipeTrips : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+@"DELETE FROM BoardingPassenger
+DELETE FROM BoardingPassTrip
+DELETE FROM BoardingPass
+DELETE FROM Trip");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
