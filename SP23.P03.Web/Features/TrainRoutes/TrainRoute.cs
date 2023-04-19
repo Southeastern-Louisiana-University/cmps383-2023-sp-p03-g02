e8d71ad2bab8bb48799581a5de08fdcbfc3b28b8
Jared Talbot <jared.talbot-2@selu.edu>
2023-04-19 16:26:12 -0500

    fix; Fix train route pricing
    paying 25 cents for a trip is a little low

--- SP23.P03.Web/Features/TrainRoutes/TrainRoute.cs
+++ SP23.P03.Web/Features/TrainRoutes/TrainRoute.cs
@@ -14,9 +14,9 @@ namespace SP23.P03.Web.Features.TrainRoutes
         public required TrainStation StationB { get; set; }
         [NotMapped]
         public TrainStation[] Stations => new TrainStation[] { StationA, StationB };
-        public int CoachPrice => (int)(DistanceMiles * 0.5);
-        public int FirstClassPrice => (int)(DistanceMiles);
-        public int RoomletPrice => (int)(DistanceMiles * 2);
-        public int SleeperPrice => (int)(DistanceMiles * 3);
+        public int CoachPrice => (int)(DistanceMiles * 50);
+        public int FirstClassPrice => (int)(DistanceMiles * 100);
+        public int RoomletPrice => (int)(DistanceMiles * 200);
+        public int SleeperPrice => (int)(DistanceMiles * 300);
     }
 }
