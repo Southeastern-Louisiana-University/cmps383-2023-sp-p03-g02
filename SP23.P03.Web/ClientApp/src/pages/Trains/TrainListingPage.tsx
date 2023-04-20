26fd5421f53d31353c1b5ee035b92dab446027f1
Zoe Klein <zoe.klein@selu.edu>
2023-04-19 21:17:15 -0500

    wip; designing page

--- SP23.P03.Web/ClientApp/src/pages/Trains/TrainListingPage.tsx
+++ SP23.P03.Web/ClientApp/src/pages/Trains/TrainListingPage.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Table } from "semantic-ui-react";
+import './TrainListingPage.css';
 
 export function TrainListingPage(): React.ReactElement {
     return (
-        <>
-            Here is our info!
-        </>
+        <Table columns={2} padded className="render-table">
+            <Table.Header className="ui center aligned">
+                <Table.HeaderCell> Train </Table.HeaderCell>
+                <Table.HeaderCell> Actions </Table.HeaderCell>
+            </Table.Header>
+
+            <Table.Body>
+                <Table.Cell>
+                    cell 1
+                </Table.Cell>
+                <Table.Cell>
+                    cell 2
+                </Table.Cell>
+            </Table.Body>
+        </Table>
     )
 }
 
