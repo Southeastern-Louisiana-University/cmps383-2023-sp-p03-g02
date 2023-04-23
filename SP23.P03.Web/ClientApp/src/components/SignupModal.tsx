6564b6229a9d5c35e9a771727146b60832ec0ff1
Jacob Poirrier <jakep0802@gmail.com>
2023-04-23 18:38:00 -0500

    feat: Sign up modal

--- SP23.P03.Web/ClientApp/src/components/SignupModal.tsx
+++ SP23.P03.Web/ClientApp/src/components/SignupModal.tsx
@@ -9,6 +9,40 @@ export const openSignupModal = () => {
     notify("open-signup-modal");
 }
 
+function Password(): ReactElement {
+    const [password, setPassword] = useState({
+        password: '',
+        confirmPassword: ''
+    })
+
+    const [match, setMatch] = useState(false);
+
+    const inputChange: (event: React.ChangeEvent<HTMLInputElement>) => void = (event) => {
+        const { value, name } = event.target;
+        setPassword({
+            ...password,
+            [name]: value
+        }) 
+    }
+
+    useEffect(() => {
+        setMatch(!!password.password && password.confirmPassword === password.confirmPassword)
+    }, [password])
+
+    return (
+        <div>
+            <div className="field-label">
+                <label htmlFor="password">Password</label>
+            </div>
+            <Field as={Input} id="password" name="password" type="password" className="field" />
+            <div className="field-label">
+                <label htmlFor="confirm-password">Confirm Password</label>
+            </div>
+            <Field as={Input} onChange={inputChange} id="confirmPassword" name="confirmPassword" type="password" className="field" />
+        </div>
+    )
+}
+
 const SignupModal = (props: ModalProps) => {
     const [signupOpen, setSignupOpen] = useState(false);
     const [loading, setLoading] = useState(false);
@@ -28,11 +62,12 @@ const SignupModal = (props: ModalProps) => {
             .catch((error) => {
                 setLoading(false);
                 console.error(error);
-                alert("Signup failed.");
+                //alert("Signup failed.");
             });
     }
 
     const INITIAL_VALUES: CreateUserDto = {userName: "", password: "", roles: ["user"]}
+
     return (
         <Formik initialValues={INITIAL_VALUES} onSubmit={onSubmit}>
             <Modal {...props}
@@ -51,14 +86,7 @@ const SignupModal = (props: ModalProps) => {
                         <label htmlFor="userName">Username</label>
                     </div>
                     <Field as={Input} id="userName" name="userName" className="field" />
-                    <div className="field-label">
-                        <label htmlFor="password">Password</label>
-                    </div>
-                    <Field as={Input} id="password" name="password" type="password" className="field" />
-                    <div className="field-label">
-                        <label htmlFor="confirm-password">Confirm Password</label>
-                    </div>
-                    <Field as={Input} id="confirm-password" name="confirm-password" type="password" className="field" />
+                    {Password()}
                 </Modal.Content>
                 <Modal.Actions>
                     <Button type="submit" color="green" loading={loading}>
