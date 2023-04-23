947ed6b435310f53b07c6bc08ddc86ea62d9c3e9
Jared Talbot <jared.talbot-2@selu.edu>
2023-04-23 16:48:20 -0500

    feat; TrainRoutes controller, GET request, dto

---
+++ SP23.P03.Web/Controllers/TrainRoutesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SP23.P03.Web.Data;
+using SP23.P03.Web.Features.TrainRoutes;
+using SP23.P03.Web.Features.TrainStations;
+
+namespace SP23.P03.Web.Controllers;
+
+
+[Route("api/trainRoutes")]
+[ApiController]
+public class TrainRoutesController : ControllerBase
+{
+    private readonly DbSet<TrainRoute> trainRoutes;
+    private readonly DataContext dataContext;
+    public TrainRoutesController(DataContext dataContext)
+    {
+        this.dataContext = dataContext;
+        trainRoutes = dataContext.Set<TrainRoute>();
+    }
+
+    [HttpGet]
+    public ActionResult<ICollection<TrainRouteDto>> GetTrainRoutes()
+    {
+        return Ok(GetTrainRouteDtos(trainRoutes));
+    }
+
+    private static IQueryable<TrainRouteDto> GetTrainRouteDtos(IQueryable<TrainRoute> trainRoutes) =>
+        trainRoutes.Select(x => new TrainRouteDto
+        {
+            DistanceMiles = x.DistanceMiles,
+            EstimatedMinutes = x.EstimatedMinutes,
+            GeographyMultiplier = x.GeographyMultiplier,
+            StationA = new TrainStationDto
+            {
+                Id = x.StationA.Id,
+                Name = x.StationA.Name,
+                Address = x.StationA.Address,
+                ManagerId = x.StationA.ManagerId,
+            },
+            StationB = new TrainStationDto
+            {
+                Id = x.StationB.Id,
+                Name = x.StationB.Name,
+                Address = x.StationB.Address,
+                ManagerId = x.StationB.ManagerId,
+            },
+            CoachPrice = x.CoachPrice,
+            FirstClassPrice = x.FirstClassPrice,
+            RoomletPrice = x.RoomletPrice,
+            SleeperPrice = x.SleeperPrice,
+        });
+}
---
+++ SP23.P03.Web/Features/TrainRoutes/TrainRouteDto.cs
@@ -0,0 +1,16 @@
+﻿using SP23.P03.Web.Features.TrainStations;
+
+namespace SP23.P03.Web.Features.TrainRoutes;
+
+public class TrainRouteDto
+{
+    public double DistanceMiles { get; set; }
+    public double EstimatedMinutes { get; set; }
+    public double GeographyMultiplier { get; set; }
+    public required TrainStationDto StationA { get; set; }
+    public required TrainStationDto StationB { get; set; }
+    public int CoachPrice { get; set; }
+    public int FirstClassPrice { get; set; }
+    public int RoomletPrice { get; set; }
+    public int SleeperPrice { get; set; }
+}
