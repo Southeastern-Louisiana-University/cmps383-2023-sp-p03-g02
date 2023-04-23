f2bc66661da9a6212d53740d19f85ae61d9f55b6
Zoe Klein <zoe.klein@selu.edu>
2023-04-23 18:23:56 -0500

    remove unneeded code

--- SP23.P03.Web/ClientApp/src/pages/Account/AccountManage.tsx
+++ SP23.P03.Web/ClientApp/src/pages/Account/AccountManage.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
-import { Button, Container, Header, Icon, Input, Modal, Popup, Segment, Table } from "semantic-ui-react";
+import React from "react";
+import { Button, Container, Header, Icon, Popup, Segment, Table } from "semantic-ui-react";
 import { useUser } from "../../components/AuthProvider";
-import { Field, Form } from "formik";
 
 export function AccountManage(): React.ReactElement {
     const user = useUser();
-    const [open, setOpen] = useState(false);
 
     return (
         <Container>
@@ -48,27 +46,9 @@ export function AccountManage(): React.ReactElement {
                 <Table.Body className="ui center aligned">
                     <Table.Cell> Password </Table.Cell>
                     <Table.Cell>
-                    <Modal
-                        as={Form}
-                        trigger={
-                            <Button color="yellow">
-                                <Icon name="edit" compact />
-                            </Button>
-                        }
-                        onClose={() => setOpen(false)}
-                        onOpen={() => setOpen(true)}
-                        open={open}
-                    >
-                        <div>
-                            <h2> Change Password </h2>
-                            <div>
-                                <label><b> Password </b></label>
-                                <Field as={Input} id="password" name="password" />
-                            </div>
-                            <Button negative type="button" onClick={() => setOpen(false)}> Cancel </Button>
-                            <Button positive type="submit"> Change </Button>
-                        </div>
-                    </Modal>
+                        <Button color="yellow">
+                            <Icon name="edit" compact />
+                        </Button>
                     </Table.Cell>
                 </Table.Body>
                 </Table>
